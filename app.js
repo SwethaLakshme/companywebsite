981c6a3643ff4bd8c3a5f030f07ba9a1ddd99bd2
santhoshunt <santhoshkumar2012002@gmail.com>
2021-05-09 19:04:53 +0530

    contact form speed

--- app.js
+++ app.js
@@ -1,72 +1,68 @@
-TweenMax.to(".full-page-layer", 2, {
-    scaleX: 1,
-    delay: 0,
-    opacity: 1,
-    ease: Power2.easeInOut
+TweenMax.to(".full-page-layer", 1, {
+  scaleX: 1,
+  delay: 0,
+  opacity: 1,
+  ease: Power2.easeInOut,
 });
-TweenMax.to(".hero-section-img", 1.6, {
-    scaleY: 1,
-    delay: 0,
-    opacity: 1,
-    ease: Power2.easeInOut
+TweenMax.to(".hero-section-img", 1, {
+  scaleY: 1,
+  delay: 0,
+  opacity: 1,
+  ease: Power2.easeInOut,
 });
-TweenMax.to(".landing-page-layer-1 ", 1.6, {
-    scaleY: 0,
-    delay: 0.2,
-    opacity: 1,
-    ease: Power2.easeInOut
-});
-
-TweenMax.to(".landing-page-layer-2", 1.6, {
-    scaleY: 0,
-    delay: 0.4,
-    opacity: 1,
-    ease: Power2.easeInOut
+TweenMax.to(".landing-page-layer-1 ", 1, {
+  scaleY: 0,
+  delay: 0.1,
+  opacity: 1,
+  ease: Power2.easeInOut,
 });
 
-
-TweenMax.to(".Landing-page-policy", 1.6, {
-    scaleY: 1,
-    delay: 0,
-    opacity: 1,
-    ease: Power2.easeInOut
+TweenMax.to(".landing-page-layer-2", 1, {
+  scaleY: 0,
+  delay: 0.2,
+  opacity: 1,
+  ease: Power2.easeInOut,
 });
 
+TweenMax.to(".Landing-page-policy", 1, {
+  scaleY: 1,
+  delay: 0,
+  opacity: 1,
+  ease: Power2.easeInOut,
+});
 
 TweenMax.to("#contact", 1, {
-    scaleX: 1,
-    delay: 0,
-    opacity: 1,
-    ease: Power2.easeInOut
+  scaleX: 1,
+  delay: 0,
+  opacity: 1,
+  ease: Power2.easeInOut,
 });
 
-TweenMax.to(".containers", 2, {
-    scaleX: 1,
-    delay: 0,
-    opacity: 1,
-    ease: Power2.easeInOut
+TweenMax.to(".containers", 1, {
+  scaleX: 1,
+  delay: 0,
+  opacity: 1,
+  ease: Power2.easeInOut,
 });
 
 TweenMax.to(".cont-info", 1, {
-    scaleX: 1,
-    delay: 1,
-    opacity: 1,
-    ease: Power2.easeInOut
-}, );
-
+  scaleX: 1,
+  delay: 0.5,
+  opacity: 1,
+  ease: Power2.easeInOut,
+});
 
 TweenMax.from(".cont-info-head h2", 1, {
-    y: "-30",
-    opacity: 1,
-    ease: Power2.easeInOut,
-    delay: 3
+  y: "-30",
+  opacity: 1,
+  ease: Power2.easeInOut,
+  delay: 1
 });
 TweenMax.from(".column-left p", 1, {
-    y: "-30",
-    opacity: 0,
-    ease: Power2.easeInOut,
-    dey: 3
+  y: "-30",
+  opacity: 0,
+  ease: Power2.easeInOut,
+  delay: 1,
 });
-
 
 //carousel
